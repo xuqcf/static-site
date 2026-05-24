574d820c0abbf22115f17ebb825cd2a8b9149558
xuqcf <sevt33nclothingbrand@gmail.com>
2026-05-24 14:05:38 -0400

    added more functionality

--- src/textnode.py
+++ src/textnode.py
@@ -77,14 +77,10 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     return new_nodes
 
 def extract_markdown_images(text):
-    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
-    print(extract_markdown_images(text))
-    # [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
 
 def extract_markdown_links(text):
-    text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
-    print(extract_markdown_links(text))
-    # [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+    return re.findall(r"(?<!!)\[(.*?)\]\((.*?)\)", text)
 
 def split_nodes_image(old_nodes):
     new_nodes = []
@@ -141,20 +137,20 @@ def split_nodes_link(old_nodes):
             continue
 
         original_text = node.text
-        images = extract_markdown_images(original_text)
+        links = extract_markdown_links(original_text)
 
         # If no images, keep original node
-        if len(link) == 0:
+        if len(links) == 0:
             new_nodes.append(node)
             continue
 
         remaining_text = original_text
 
-        for link_alt, link_url in link:
+        for link_alt, link_url in links:
 
-            image_markdown = f"![{link_alt}]({link_url})"
+            link_markdown = f"[{link_alt}]({link_url})"
 
-            sections = remaining_text.split(image_markdown, 1)
+            sections = remaining_text.split(link_markdown, 1)
 
             # Text before image
             if sections[0] != "":
@@ -162,7 +158,7 @@ def split_nodes_link(old_nodes):
 
             # LINK node
             new_nodes.append(
-                TextNode(image_alt, TextType.LINK, link_url))
+                TextNode(link_alt, TextType.LINK, link_url))
 
             # Continue processing the remaining text
             remaining_text = sections[1]
@@ -172,3 +168,19 @@ def split_nodes_link(old_nodes):
             new_nodes.append(TextNode(remaining_text, TextType.TEXT))
 
     return new_nodes
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+
+    nodes = split_nodes_image(nodes)
+    
+    nodes = split_nodes_link(nodes)
+
+    return nodes
