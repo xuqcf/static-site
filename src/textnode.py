396e7bd128208b15df035fe80064b206a86d8d33
xuqcf <sevt33nclothingbrand@gmail.com>
2026-05-23 22:03:05 -0400

    jsanbd

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,7 @@
 from enum import Enum
 from htmlnode import LeafNode
+import re
+
 
 class TextType(Enum):
     TEXT = "text"
@@ -46,3 +48,45 @@ def text_node_to_html_node(text_node):
 
     else:
         raise Exception("Invalid TextType")
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    
+    new_nodes = []
+
+    for nodes in old_nodes:
+
+        if nodes.text_type != TextType.TEXT:
+            new_nodes.append(nodes)
+            continue
+
+        parts = nodes.text.split(delimiter)
+        
+        if len(parts) % 2 == 0:
+            raise Exception("invalid markdown")
+
+        for i in range(len(parts)):
+
+            if parts[i] == "":
+                continue
+
+            if i % 2 == 0:
+                new_nodes.append(TextNode(parts[i], TextType.TEXT))
+            else:
+                new_nodes.append(Te1xtNode(parts[i], text_type))
+
+    return new_node
+
+def extract_markdown_images(text):
+    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
+    print(extract_markdown_images(text))
+    # [("rick roll", "https://i.imgur.com/aKaOqIh.gif"), ("obi wan", "https://i.imgur.com/fJRm4Vk.jpeg")]
+
+def extract_markdown_links(text):
+    text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+    print(extract_markdown_links(text))
+    # [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
+
+def split_nodes_image(old_nodes):
+
+    new_node = []
+    matches = re.findall(r+![+,text)
