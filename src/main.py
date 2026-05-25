08176b1eaa1825664a04b745b4f407c5af1f7910
xuqcf <sevt33nclothingbrand@gmail.com>
2026-05-25 14:34:29 -0400

    added generate page function and some more functionality and files

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, markdown_to_html_node
 import os 
 import shutil 
 import re
@@ -9,6 +9,8 @@ def main():
 
     prepare_public(public)
     copy_dir(static, public)
+
+generate_page("content/index.md", "template.html", "public/index.html")
 
 def prepare_public(public_path):
     if os.path.exists(public_path):
@@ -40,5 +42,28 @@ def extract_title(markdown):
             return cleaned_line[1:].strip()
         
     raise ValueError("No H1 heading found in the provided markdown.")
+
+def generate_page(from_path, template_path, dest_path):
+    
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    
+    with open(from_path, "r") as f:
+        from_content = f.read
+    
+    with open(template_content, "r") as f:
+        template_content = f.read
+    
+    html_node = markdown_to_html_node(from_content)
+    
+    string = html_node.to_html
+    
+    title = extract_title(from_content)
+
+    template_content = template_content.replace("{{ Title }}", title)
+    template_content = template_content.replace("{{ Content }}", string)
+
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+
+    
 if __name__ == "__main__":
     main()
