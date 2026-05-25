afa99359188bfb6f5f08538f1744a3cb2ea442f2
xuqcf <sevt33nclothingbrand@gmail.com>
2026-05-25 13:49:04 -0400

    cleaned up code and added some more stuff

--- src/main.py
+++ src/main.py
@@ -3,37 +3,32 @@ import os
 import shutil 
 
 def main():
-    node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(node)
+    static = "static"
+    public = "public"
 
-static = "static"
-public = "public"
+    prepare_public(public)
+    copy_dir(static, public)
 
-def prepare_public(static, public):
+def prepare_public(public_path):
     if os.path.exists(public):
         shutil.rmtree(public)
 
     os.mkdir(public)
 
-def copy_dir(static, public):
+def copy_dir(source_dir, dest_dir):
+ 
+    for item in os.listdir(source_dir):
+        source_path = os.path.join(source_dir, item) #this looks inside source folder for the item and gives a path - source 
 
-    content = os.listdir(static)
+        dest_path = os.path.join(dest_dir, item) #this looks inside the public folder and makes a destination 
 
-    source = ""
-    destination = ""
-    
-    
-    for item in content:
-        source = os.path.join(static, item) #this looks inside source folder for the item and gives a path - source 
-
-        destination = os.path.join(public, item) #this looks inside the public folder and makes a destination 
-
-        if os.path.isfile(source):
-            shutil.copy(source, destination)
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, destination)
+            print(f"Copied file: {source_path} -> {dest_path}")
+        
         else:
-            os.mkdir(destination)
-            copy_dir(source, destination)
+            os.mkdir(dest_path)
+            copy_dir(source_path, dest_path)
 
-main()
-prepare_public(static, public)
-copy_dir(static, public)
+if __name__ == "__main__":
+    main()
