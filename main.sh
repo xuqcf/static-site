33ba467f8597a98c8ef364abfdea55473cd3d045
xuqcf <sevt33nclothingbrand@gmail.com>
2026-05-25 16:45:35 -0400

    added a bunch of stuff and fixed and improved

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python3 src/main.py
-cd public && python3 -m http.server 8888
+cd doc && python3 -m http.server 8888
--- src/main.py
+++ src/main.py
@@ -2,15 +2,22 @@ from textnode import TextNode, TextType, markdown_to_html_node
 import os 
 import shutil 
 import re
+import sys
 
 def main():
+
+    if len(sys.argv) >1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+
     static = "static"
-    public = "public"
+    doc = "doc"
 
-    prepare_public(public)
-    copy_dir(static, public)
+    prepare_public(doc)
+    copy_dir(static, doc)
 
-    generate_page("content/index.md", "template.html", "public/index.html")
+    generate_pages_recursive("content", "template.html", "doc", basepath)
 
 def prepare_public(public_path):
     if os.path.exists(public_path):
@@ -43,9 +50,9 @@ def extract_title(markdown):
         
     raise ValueError("No H1 heading found in the provided markdown.")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     
-    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    print(f"Generating page: {from_path} to {dest_path} using {template_path}")
     
     with open(from_path, "r") as f:
         from_content = f.read()
@@ -62,10 +69,34 @@ def generate_page(from_path, template_path, dest_path):
     template_content = template_content.replace("{{ Title }}", title)
     template_content = template_content.replace("{{ Content }}", string)
 
+    template_content = template_content.replace('href="/', f'href="{basepath}')
+    template_content = template_content.replace('src="/', f'src="{basepath}')
+
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
     with open(dest_path, "w") as f:
         f.write(template_content)
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
+    for item in os.listdir(dir_path_content):
+        source_path = os.path.join(dir_path_content, item) #this looks inside content folder for the item and gives a path - source 
+
+        dest_path = os.path.join(dest_dir_path, item) #this looks inside the public folder and makes a destination 
+
+        if os.path.isfile(source_path):
+            
+            if source_path.endswith(".md"):
+                
+                new_dest = dest_path.replace(".md", ".html")
+
+                generate_page(source_path, template_path, new_dest, basepath)
+
+            print(f"Copied file: {source_path} -> {dest_path}")
+        else:
+            os.makedirs(dest_path, exist_ok=True)
+
+            generate_pages_recursive(source_path, template_path, dest_path, basepath)
+    
 
 if __name__ == "__main__":
     main()
